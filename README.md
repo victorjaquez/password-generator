92da5ed6e8360b45d6be0e91817b5f92f16e3e58
victorjaquez <victor@jaquez.me>
2019-01-29 11:38:59 -0700

    requirements, git

---
+++ README.md
@@ -0,0 +1,7 @@
+# Requirements
+
+- 8 - 32 characters
+- 1 Uppercase
+- 1 Lowercase
+- 1 Special Character
+- 1 Number
---
+++ index.js
@@ -0,0 +1,6 @@
+// Requirements
+// 8 - 32 characters
+// 1 Uppercase
+// 1 Lowercase
+// 1 Special Character
+// 1 Number
